1981340336c15f649cf70405f820c7217e49ef0a
Maximo De Leon <maximodleon@gmail.com>
2024-09-19 22:40:21 -0400

    Support multiple clients

--- app/server.c
+++ app/server.c
@@ -1,66 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <poll.h>
+#define MAX_CLIENTS 100
 
 int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
-	setbuf(stderr, NULL);
-	
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+  // Disable output buffering
+  setbuf(stdout, NULL);
+  setbuf(stderr, NULL);
 
-	 int server_fd, client_addr_len;
-	 struct sockaddr_in client_addr;
-	
-	 server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	 if (server_fd == -1) {
-	 	printf("Socket creation failed: %s...\n", strerror(errno));
-	 	return 1;
-	 }
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  printf("Logs from your program will appear here!\n");
 
-	// // Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// // ensures that we don't run into 'Address already in use' errors
-	 int reuse = 1;
-	 if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-	 	printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-	 	return 1;
-	 }
+   int server_fd;
+   socklen_t client_addr_len;
+   struct sockaddr_in client_addr;
+   struct pollfd pollfds[MAX_CLIENTS];
 
-	 struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-	 								 .sin_port = htons(6379),
-	 								 .sin_addr = { htonl(INADDR_ANY) },
-	 								};
-
-	 if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-	 	printf("Bind failed: %s \n", strerror(errno));
-	 	return 1;
-	 }
-
-	 int connection_backlog = 5;
-	 if (listen(server_fd, connection_backlog) != 0) {
-	 	printf("Listen failed: %s \n", strerror(errno));
-	 	return 1;
-	 }
-
-	 printf("Waiting for a client to connect...\n");
-	 client_addr_len = sizeof(client_addr);
-	
-   char *pong = "+PONG\r\n";
-
-   int clientfd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-   char buff[1000];
-
-   while(recv(clientfd, buff, 1000, 0) != -1) {
-    write(clientfd, pong, strlen(pong));
+   server_fd = socket(AF_INET, SOCK_STREAM, 0);
+   if (server_fd == -1) {
+    printf("Socket creation failed: %s...\n", strerror(errno));
+    return 1;
    }
 
-   close(clientfd);
-	 close(server_fd);
-	 return 0;
+  // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // // ensures that we don't run into 'Address already in use' errors
+   int reuse = 1;
+   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+    return 1;
+   }
+
+   struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+                   .sin_port = htons(6379),
+                   .sin_addr = { htonl(INADDR_ANY) },
+                  };
+
+   if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+    printf("Bind failed: %s \n", strerror(errno));
+    return 1;
+   }
+
+   int connection_backlog = 5;
+   if (listen(server_fd, connection_backlog) != 0) {
+    printf("Listen failed: %s \n", strerror(errno));
+    return 1;
+   }
+
+   printf("Waiting for a client to connect...\n");
+   client_addr_len = sizeof(client_addr);
+
+   char *pong = "+PONG\r\n";
+
+   char buff[1000];
+
+  pollfds[0].fd = server_fd;
+  pollfds[0].events = POLLIN;
+  int nfds = 1;
+
+  // initialize pollfds array
+  for (int i = 1; i < MAX_CLIENTS; i++) {
+    pollfds[i].fd = 0;
+  }
+
+   while(1) {
+    int bytes = poll(pollfds, nfds, -1);
+    if (bytes > 0) {
+      if(pollfds[0].revents && POLLIN) {
+        int clientfd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+        printf("accept successful: %i\n", clientfd);
+
+        for (int i = 1; i < MAX_CLIENTS; i++) {
+          // if slot available
+          // set it
+          if(pollfds[i].fd == 0) {
+            pollfds[i].fd = clientfd;
+            pollfds[i].events = POLLIN;
+            nfds++;
+            break;
+          }
+        }
+      }
+    }
+
+    for (int i = 1; i < MAX_CLIENTS; i++) {
+      // if we get a POLLIN event and the slot is not empty
+      // handle it
+      if(pollfds[i].revents && pollfds[i].fd != 0 && POLLIN) {
+        int buf_size = read(pollfds[i].fd, buff, 1000);
+        if(buf_size == -1 || buf_size == 0) {
+           pollfds[i].fd = 0;
+           pollfds[i].events = 0;
+           pollfds[i].revents = 0;
+           nfds--;
+        } else {
+          write(pollfds[i].fd, pong, strlen(pong));
+        }
+      }
+    }
+   }
+
+   close(server_fd);
+   return 0;
 }
